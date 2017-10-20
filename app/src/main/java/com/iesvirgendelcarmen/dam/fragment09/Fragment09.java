04519b75b6aea30d09aa69c59ad5ab614bc7b91f
jaestrella <jaestrella1991@gmail.com>
2017-10-20 12:54:58 +0200

    subiendo proyecto

---
+++ app/src/main/java/com/iesvirgendelcarmen/dam/fragment09/Fragment09.java
@@ -0,0 +1,15 @@
+package com.iesvirgendelcarmen.dam.fragment09;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Fragment09 extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragmento1);
+    }
+}
---
+++ app/src/main/java/com/iesvirgendelcarmen/dam/fragment09/Fragmento1.java
@@ -0,0 +1,20 @@
+package com.iesvirgendelcarmen.dam.fragment09;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by matinal on 20/10/2017.
+ */
+
+public class Fragmento1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragmento1, container, false);
+    }
+}
