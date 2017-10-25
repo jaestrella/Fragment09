755b65a18d2a62e0a8bed9a4787f9853a02d7058
jaestrella <jaestrella1991@gmail.com>
2017-10-25 12:40:25 +0200

    subiendo

--- app/src/main/java/com/iesvirgendelcarmen/dam/fragment09/Fragment09.java
+++ app/src/main/java/com/iesvirgendelcarmen/dam/fragment09/Fragment09.java
@@ -2,14 +2,31 @@ package com.iesvirgendelcarmen.dam.fragment09;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Fragment09 extends FragmentActivity {
 
+    Adaptador paginador;
+    ViewPager visor;
+    ArrayList<Fragment>listaFragmentos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragmento1);
+        setContentView(R.layout.activity_fragment09);
+
+        listaFragmentos=new ArrayList<Fragment>();
+        listaFragmentos.add(new Fragmento3());
+        listaFragmentos.add(new Fragmento1());
+        listaFragmentos.add(new Fragmento3());
+        listaFragmentos.add(new Fragmento2());
+        listaFragmentos.add(new Fragmento3());
+        paginador=new Adaptador(getSupportFragmentManager(),listaFragmentos);
+        visor=(ViewPager)findViewById(R.id.viewPager);
+        visor.setAdapter(paginador);
     }
 }
---
+++ app/src/main/java/com/iesvirgendelcarmen/dam/fragment09/Fragmento3.java
@@ -0,0 +1,41 @@
+package com.iesvirgendelcarmen.dam.fragment09;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+
+/**
+ * Created by matinal on 25/10/2017.
+ */
+
+public class Fragmento3 extends ListFragment {
+    int tipoLista=(int) (Math.random()*3);
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragmento3, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setListAdapter(new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,Contenido.pintores));
+        switch(tipoLista) {
+            case0:
+            setListAdapter(new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,Contenido.pintores));
+            break;
+            case1:
+            setListAdapter(new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,Contenido.escultores));
+            break;
+            case2:
+            setListAdapter(new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,Contenido.arquitectos));
+            break;
+        }
+    }
+
+}
