296193c1a69ba22aa232261ec7a62921f3343bea
jaestrella <jaestrella1991@gmail.com>
2017-10-25 12:42:13 +0200

    subiendo

---
+++ app/src/main/java/com/iesvirgendelcarmen/dam/fragment09/Contenido.java
@@ -0,0 +1,11 @@
+package com.iesvirgendelcarmen.dam.fragment09;
+
+/**
+ * Created by matinal on 25/10/2017.
+ */
+
+public class Contenido {
+    public static final String[] pintores = { "RENOIR", "REMBRANT",  "GAUGUIN", "BOTICELLI", "RAFAEL","MASACCIO", "DELACROIX", "VELAZQUEZ" };
+    public static final String[] escultores = { "MIGUEL ANGEL", "RODIN", "DONATELLO", "BERNINI", "BRANCUSI", "FIDIAS","CANOVA", "MOORE", "THORVALDSEN" };
+    public static final String[] arquitectos = { "GAUDÍ", "BRUNELLESCHI", "BORROMINI", "VAN DER ROHE", "LE CORBUSIER", "NORMAN FOSTER",  "LLOYD WRIGHT", "VASARI"};
+}
